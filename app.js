f4bd32253391534174631fe1616cdb0541e70f2e
amanda lacerda <amanda.oliveira.lacerda@escola.pr.gov.br>
2026-04-27 18:01:14 +0000

    3

--- app.js
+++ app.js
@@ -3,6 +3,14 @@
 const switcher = document.querySelector('.botao');
 
 switcher.addEventListener('click', function(){
-    document.body.classList.t
+    document.body.classList.toggle('tema-escuro');
+     document.body.classList.toggle('tema-claro');
 
-}
+     let nomeDaClasse= document.body.className;
+     if (nomeDaClasse.includes("tema-claro")){
+        this.texrcontent = "escuro";
+     }else{
+      this.texrcontent = "claro";
+     }
+
+});
